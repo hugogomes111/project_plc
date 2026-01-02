3752e07c7e1e12ee2c80a739694d5a74d358f67b
Vitor Pereira <135645353+vitorcpereira@users.noreply.github.com>
2026-01-02 21:18:06 +0000

    Update anasinv4.py

--- anasinv4.py
+++ anasinv4.py
@@ -2,10 +2,9 @@ import ply.yacc as yacc
 from analexerv4 import *
 
 """
-VERSÃO SIMPLIFICADA - SEM GESTÃO DE ESCOPOS
-- Mais fácil de entender
-- Funciona para exemplos 1-4
-- NÃO funciona bem com funções (exemplo 5)
+COMPILADOR PASCAL CORRIGIDO
+- Usa PUSHN para inicialização de arrays
+- parser.arrays guarda posições iniciais dos arrays
 """
 
 def p_Program(t):
@@ -87,11 +86,12 @@ def p_VarList(t):
         if isinstance(var_type, tuple) and var_type[0] == 'array':
             array_size = var_type[2] - var_type[1] + 1
             element_type = var_type[3]
+            #posicao inicial do array
+            parser.arrays[var_name] = parser.varcount
             parser.var[var_name] = parser.varcount
             parser.vartype[var_name] = var_type
-            for _ in range(array_size):
-                pushcode.append(parser.pushdict.get(element_type, 'pushi 0\n'))
-                parser.varcount += 1
+            pushcode.append(f'pushn {array_size}\n')
+            parser.varcount += array_size
         else:
             # Variável simples
             parser.var[var_name] = parser.varcount
@@ -146,31 +146,31 @@ def p_Type_array(t):
 def p_Functions(t):
     r'Functions : FUNCTION ID "(" Parameters ")" ":" Type ";" FunctionBody'
     func_name = t[2]
-    func_label = f'func_{func_name}'
+    func_label = f'func{func_name}'
     
     parser.functions[func_name] = func_label
     
     # Gera código
-    code = [f'jump end_{func_label}\n']
+    code = [f'jump end{func_label}\n']
     code.append(f'{func_label}:\n')
     code.extend(t[9])
     code.append('return\n')
-    code.append(f'end_{func_label}:\n')
+    code.append(f'end{func_label}:\n')
     
     t[0] = code
 
 def p_Functions_no_params(t):
     r'Functions : FUNCTION ID ":" Type ";" FunctionBody'
     func_name = t[2]
-    func_label = f'func_{func_name}'
+    func_label = f'func{func_name}'
     
     parser.functions[func_name] = func_label
     
-    code = [f'jump end_{func_label}\n']
+    code = [f'jump end{func_label}\n']
     code.append(f'{func_label}:\n')
     code.extend(t[6])
     code.append('return\n')
-    code.append(f'end_{func_label}:\n')
+    code.append(f'end{func_label}:\n')
     
     t[0] = code
 
@@ -182,30 +182,30 @@ def p_FunctionBody(t):
 def p_Procedures(t):
     r'Procedures : PROCEDURE ID "(" Parameters ")" ";" ProcedureBody'
     proc_name = t[2]
-    proc_label = f'proc_{proc_name}'
+    proc_label = f'proc{proc_name}'
     
     parser.functions[proc_name] = proc_label
     
-    code = [f'jump end_{proc_label}\n']
+    code = [f'jump end{proc_label}\n']
     code.append(f'{proc_label}:\n')
     code.extend(t[7])
     code.append('return\n')
-    code.append(f'end_{proc_label}:\n')
+    code.append(f'end{proc_label}:\n')
     
     t[0] = code
 
 def p_Procedures_no_params(t):
     r'Procedures : PROCEDURE ID ";" ProcedureBody'
     proc_name = t[2]
-    proc_label = f'proc_{proc_name}'
+    proc_label = f'proc{proc_name}'
     
     parser.functions[proc_name] = proc_label
     
-    code = [f'jump end_{proc_label}\n']
+    code = [f'jump end{proc_label}\n']
     code.append(f'{proc_label}:\n')
     code.extend(t[4])
     code.append('return\n')
-    code.append(f'end_{proc_label}:\n')
+    code.append(f'end{proc_label}:\n')
     
     t[0] = code
 
@@ -267,7 +267,6 @@ def p_WriteItem_exp(t):
     r'WriteItem : Exp'
     t[0] = t[1] + ['writei\n']
 
-# I/O - READLN
 def p_Block_readln(t):
     r'Block : READLN "(" ID ")" ";"'
     if t[3] in parser.var:
@@ -285,133 +284,169 @@ def p_Block_readln(t):
         t[0] = []
 
 def p_Block_readln_array(t):
-    r'Block : READLN "(" ID "[" Exp "]" ")" ";"'
-    if t[3] in parser.var:
-        var_offset = parser.var[t[3]]
-        t[0] = t[5] + [f'pushg {var_offset}\npadd\nread\natoi\nstoreg 0\n']
+    r'Block : READLN "(" ID "[" Factor "]" ")" ";"'
+    if t[3] in parser.arrays:
+        resto = "read\n"
+        array_info = parser.vartype[t[3]]
+        var_type = array_info[3]
+        if var_type == "integer":
+            resto += "atoi\n"
+        elif var_type =="real":
+            resto += "atof\n"
+        else:
+            pass
+        t[0] = [resto + f'storeg {parser.arrays[t[3]] + int(t[5][0][5:])}\n']
     else:
+        print(f"Erro: array '{t[3]}' não declarado")
         t[0] = []
 
-# ATRIBUIÇÃO
 def p_Block_ass(t):
     r'Block : ID ASSIGN Exp ";"'
-    if t[1] in parser.var:
+    if t[1] in parser.var and t[1] not in parser.arrays:  # Não é array
         var_offset = parser.var[t[1]]
         t[0] = t[3] + [f'storeg {var_offset}\n']
     else:
-        # Retorno de função
+        # Retorno de função ou array
         t[0] = t[3] + ['storel 0\n']
 
 def p_Block_ass_array(t):
-    r'Block : ID "[" Exp "]" ASSIGN Exp ";"'
-    if t[1] in parser.var:
-        var_offset = parser.var[t[1]]
-        t[0] = t[3] + [f'pushg {var_offset}\npadd\n'] + t[6] + ['storeg 0\n']
+    r'Block : ID "[" Factor "]" ASSIGN Exp ";"'
+    if t[1] in parser.arrays:
+        # Extrai o valor do índice
+        index_str = t[3][0].strip()
+        if index_str.startswith('pushi '):
+            index_value = int(index_str[6:])
+            array_base = parser.arrays[t[1]]
+            t[0] = t[6] + [f'storeg {array_base + index_value}\n']
+        else:
+            print(f"Erro: índice de array deve ser constante")
+            t[0] = []
     else:
+        print(f"Erro: array '{t[1]}' não declarado")
         t[0] = []
 
 # CONTROLE DE FLUXO - IF
 def p_Block_if_then(t):
     r'Block : IF Condition THEN Block'
-    label = f'label_{parser.labelcount}'
+    label = f'label{parser.labelcount}'
     parser.labelcount += 1
     t[0] = t[2] + [f'jz {label}\n'] + t[4] + [f'{label}:\n']
 
 def p_Block_if_then_else(t):
     r'Block : IF Condition THEN Block ELSE Block'
-    label_else = f'label_{parser.labelcount}'
-    label_end = f'label_{parser.labelcount + 1}'
+    label_else = f'label{parser.labelcount}'
+    label_end = f'label{parser.labelcount + 1}'
     parser.labelcount += 2
     t[0] = t[2] + [f'jz {label_else}\n'] + t[4] + [f'jump {label_end}\n', f'{label_else}:\n'] + t[6] + [f'{label_end}:\n']
 
 def p_Block_if_then_begin(t):
     r'Block : IF Condition THEN BEGIN Blocks END ";"'
-    label = f'label_{parser.labelcount}'
+    label = f'label{parser.labelcount}'
     parser.labelcount += 1
     t[0] = t[2] + [f'jz {label}\n'] + t[5] + [f'{label}:\n']
 
 def p_Block_if_then_else_begin(t):
     r'Block : IF Condition THEN BEGIN Blocks END ELSE BEGIN Blocks END ";"'
-    label_else = f'label_{parser.labelcount}'
-    label_end = f'label_{parser.labelcount + 1}'
+    label_else = f'label{parser.labelcount}'
+    label_end = f'label{parser.labelcount + 1}'
     parser.labelcount += 2
     t[0] = t[2] + [f'jz {label_else}\n'] + t[5] + [f'jump {label_end}\n', f'{label_else}:\n'] + t[9] + [f'{label_end}:\n']
+
 
 # WHILE
 def p_Block_while(t):
     r'Block : WHILE Condition DO Block'
-    label_start = f'label_{parser.labelcount}'
-    label_end = f'label_{parser.labelcount + 1}'
+    label_start = f'label{parser.labelcount}'
+    label_end = f'label{parser.labelcount + 1}'
     parser.labelcount += 2
     t[0] = [f'{label_start}:\n'] + t[2] + [f'jz {label_end}\n'] + t[4] + [f'jump {label_start}\n', f'{label_end}:\n']
 
 def p_Block_while_begin(t):
     r'Block : WHILE Condition DO BEGIN Blocks END ";"'
-    label_start = f'label_{parser.labelcount}'
-    label_end = f'label_{parser.labelcount + 1}'
+    label_start = f'label{parser.labelcount}'
+    label_end = f'label{parser.labelcount + 1}'
     parser.labelcount += 2
     t[0] = [f'{label_start}:\n'] + t[2] + [f'jz {label_end}\n'] + t[5] + [f'jump {label_start}\n', f'{label_end}:\n']
 
-# FOR
 def p_Block_for_to(t):
     r'Block : FOR ID ASSIGN Exp TO Exp DO Block'
-    if t[2] in parser.var:
+    if t[2] in parser.var and t[2] not in parser.arrays:
         var_offset = parser.var[t[2]]
-        label_start = f'label_{parser.labelcount}'
-        label_end = f'label_{parser.labelcount + 1}'
+        label_start = f'label{parser.labelcount}'
+        label_end = f'label{parser.labelcount + 1}'
         parser.labelcount += 2
         
-        t[0] = t[4] + [f'storeg {var_offset}\n', f'{label_start}:\n', f'pushg {var_offset}\n'] + t[6] + [f'inf\njz {label_end}\n'] + t[8] + [f'pushg {var_offset}\npushi 1\nadd\nstoreg {var_offset}\njump {label_start}\n', f'{label_end}:\n']
+        t[0] = t[4] + [f'storeg {var_offset}\n', 
+                      f'{label_start}:\n', 
+                      f'pushg {var_offset}\n'] + t[6] + ['pushi 1\nadd\n'] + [f'inf\njz {label_end}\n'] + t[8] + [
+                      f'pushg {var_offset}\npushi 1\nadd\nstoreg {var_offset}\n',
+                      f'jump {label_start}\n',
+                      f'{label_end}:\n']
     else:
         t[0] = []
 
 def p_Block_for_to_begin(t):
     r'Block : FOR ID ASSIGN Exp TO Exp DO BEGIN Blocks END ";"'
-    if t[2] in parser.var:
+    if t[2] in parser.var and t[2] not in parser.arrays:
         var_offset = parser.var[t[2]]
-        label_start = f'label_{parser.labelcount}'
-        label_end = f'label_{parser.labelcount + 1}'
+        label_start = f'label{parser.labelcount}'
+        label_end = f'label{parser.labelcount + 1}'
         parser.labelcount += 2
         
-        t[0] = t[4] + [f'storeg {var_offset}\n', f'{label_start}:\n', f'pushg {var_offset}\n'] + t[6] + [f'inf\njz {label_end}\n'] + t[9] + [f'pushg {var_offset}\npushi 1\nadd\nstoreg {var_offset}\njump {label_start}\n', f'{label_end}:\n']
+        t[0] = t[4] + [f'storeg {var_offset}\n', 
+                      f'{label_start}:\n', 
+                      f'pushg {var_offset}\n'] + t[6] + ['pushi 1\nadd\n'] + [f'inf\njz {label_end}\n'] + t[9] + [
+                      f'pushg {var_offset}\npushi 1\nadd\nstoreg {var_offset}\n',
+                      f'jump {label_start}\n',
+                      f'{label_end}:\n']
     else:
         t[0] = []
 
 def p_Block_for_downto(t):
     r'Block : FOR ID ASSIGN Exp DOWNTO Exp DO Block'
-    if t[2] in parser.var:
+    if t[2] in parser.var and t[2] not in parser.arrays:
         var_offset = parser.var[t[2]]
-        label_start = f'label_{parser.labelcount}'
-        label_end = f'label_{parser.labelcount + 1}'
+        label_start = f'label{parser.labelcount}'
+        label_end = f'label{parser.labelcount + 1}'
         parser.labelcount += 2
         
-        t[0] = t[4] + [f'storeg {var_offset}\n', f'{label_start}:\n', f'pushg {var_offset}\n'] + t[6] + [f'sup\njz {label_end}\n'] + t[8] + [f'pushg {var_offset}\npushi 1\nsub\nstoreg {var_offset}\njump {label_start}\n', f'{label_end}:\n']
+        t[0] = t[4] + ['pushi 1\nadd\n'] + [f'storeg {var_offset}\n', 
+                      f'{label_start}:\n', 
+                      f'pushg {var_offset}\n'] + t[6] + [f'sup\njz {label_end}\n'] + t[8] + [
+                      f'pushg {var_offset}\npushi 1\nsub\nstoreg {var_offset}\n',
+                      f'jump {label_start}\n',
+                      f'{label_end}:\n']
     else:
         t[0] = []
 
 def p_Block_for_downto_begin(t):
     r'Block : FOR ID ASSIGN Exp DOWNTO Exp DO BEGIN Blocks END ";"'
-    if t[2] in parser.var:
+    if t[2] in parser.var and t[2] not in parser.arrays:
         var_offset = parser.var[t[2]]
-        label_start = f'label_{parser.labelcount}'
-        label_end = f'label_{parser.labelcount + 1}'
+        label_start = f'label{parser.labelcount}'
+        label_end = f'label{parser.labelcount + 1}'
         parser.labelcount += 2
         
-        t[0] = t[4] + [f'storeg {var_offset}\n', f'{label_start}:\n', f'pushg {var_offset}\n'] + t[6] + [f'sup\njz {label_end}\n'] + t[9] + [f'pushg {var_offset}\npushi 1\nsub\nstoreg {var_offset}\njump {label_start}\n', f'{label_end}:\n']
+        t[0] = t[4] + ['pushi 1\nadd\n'] + [f'storeg {var_offset}\n', 
+                      f'{label_start}:\n', 
+                      f'pushg {var_offset}\n'] + t[6] + [f'sup\njz {label_end}\n'] + t[9] + [
+                      f'pushg {var_offset}\npushi 1\nsub\nstoreg {var_offset}\n',
+                      f'jump {label_start}\n',
+                      f'{label_end}:\n']
     else:
         t[0] = []
 
 # REPEAT
 def p_Block_repeat(t):
     r'Block : REPEAT Blocks UNTIL Condition ";"'
-    label_start = f'label_{parser.labelcount}'
+    label_start = f'label{parser.labelcount}'
     parser.labelcount += 1
     t[0] = [f'{label_start}:\n'] + t[2] + t[4] + [f'jz {label_start}\n']
 
 # CONDIÇÕES
 def p_Condition_or(t):
     r'Condition : Condition OR CondTerm'
-    t[0] = t[1] + t[3] + ['add\n']
+    t[0] = t[1] + t[3] + ['or\n']
 
 def p_Condition_term(t):
     r'Condition : CondTerm'
@@ -419,7 +454,7 @@ def p_Condition_term(t):
 
 def p_CondTerm_and(t):
     r'CondTerm : CondTerm AND CondFactor'
-    t[0] = t[1] + t[3] + ['mul\n']
+    t[0] = t[1] + t[3] + ['and\n']
 
 def p_CondTerm_factor(t):
     r'CondTerm : CondFactor'
@@ -444,6 +479,15 @@ def p_CondFactor_bool_true(t):
 def p_CondFactor_bool_false(t):
     r'CondFactor : FALSE'
     t[0] = ['pushi 0\n']
+
+def p_CondFactor_id(t):
+    r'CondFactor : ID'
+    if t[1] in parser.var:
+        var_offset = parser.var[t[1]]
+        t[0] = [f'pushg {var_offset}\n']
+    else:
+        print(f"Erro: '{t[1]}' não declarado")
+        t[0] = []
 
 # OPERADORES RELACIONAIS
 def p_RelOp_eq(t):
@@ -503,7 +547,7 @@ def p_Term_factor(t):
     r'Term : Factor'
     t[0] = t[1]
 
-# FATORES
+# FATORES - RESTAURADO DO CÓDIGO ANTIGO
 def p_Factor_integer(t):
     r'Factor : INT'
     t[0] = [f'pushi {t[1]}\n']
@@ -537,20 +581,20 @@ def p_Factor_id(t):
             t[0] = [f'pushs "{const_value}"\n']
         elif const_type == 'bool':
             t[0] = [f'pushi {1 if const_value else 0}\n']
-    # Verifica se é variável
-    elif t[1] in parser.var:
+    # Verifica se é variável simples (não array)
+    elif t[1] in parser.var and t[1] not in parser.arrays:
         var_offset = parser.var[t[1]]
         t[0] = [f'pushg {var_offset}\n']
     else:
-        print(f"Erro: '{t[1]}' não declarado")
+        print(f"Erro: '{t[1]}' não declarado ou é array (use indexação)")
         t[0] = []
 
 def p_Factor_array_access(t):
-    r'Factor : ID "[" Exp "]"'
-    if t[1] in parser.var:
-        var_offset = parser.var[t[1]]
-        t[0] = t[3] + [f'pushg {var_offset}\npadd\nloadn\n']
+    r'Factor : ID "[" Factor "]"'
+    if t[1] in parser.arrays:
+        t[0] = [f'pushg {parser.arrays[t[1]] + int(t[3][0][5:])}\n']
     else:
+        print(f"Erro: array '{t[1]}' não declarado")
         t[0] = []
 
 def p_Factor_function_call(t):
@@ -596,8 +640,9 @@ def p_error(t):
 lexer.lineno = 1
 parser = yacc.yacc()
 
-# Dicionários simples (SEM escopos)
-parser.var = {}           # {nome: offset}
+# Dicionários
+parser.var = {}           # {nome: offset} - para todas as variáveis
+parser.arrays = {}        # {nome_array: offset_inicial} - apenas para arrays
 parser.vartype = {}       # {nome: tipo}
 parser.constants = {}     # {nome: (tipo, valor)}
 parser.functions = {}     # {nome: label}
@@ -635,16 +680,70 @@ end.
 """
 
 ex3 = """
-program TesteConst;
-const
-    max = 100;
+program NumeroPrimo;
 var
-    x: integer;
+num, i: integer;
+primo: boolean;
 begin
-    x := max;
-    writeln('Max: ', x);
+writeln('Introduza um número inteiro positivo:');
+readln(num);
+primo := true;
+i := 2;
+while (i <= (num div 2)) and primo do
+begin
+if (num mod i) = 0 then
+primo := false;
+i := i + 1;
+end;
+if primo then
+writeln(num, ' é um número primo');
+else
+writeln(num, ' não é um número primo');
 end.
 """
+
+ex4 = """
+program SomaArray;
+var
+numeros: array[1..5] of integer;
+i, soma: integer;
+begin
+soma := 0;
+writeln('Introduza 5 números inteiros:');
+for i := 1 to 5 do
+begin
+readln(numeros[i]);
+soma := soma + numeros[i];
+end;
+writeln('A soma dos números é: ', soma);
+end.
+"""
+
+ex5 = """
+program BinarioParaInteiro;
+function BinToInt(bin: string): integer;
+var
+i, valor, potencia: integer;
+begin
+valor := 0;
+potencia := 1;
+for i := length(bin) downto 1 do
+begin
+if bin[i] = '1' then
+valor := valor + potencia;
+potencia := potencia * 2;
+end;
+BinToInt := valor;
+end;
+var
+bin: string;
+valor: integer;
+begin
+writeln('Introduza uma string binária:');
+readln(bin);
+valor := BinToInt(bin);
+writeln('O valor inteiro correspondente é: ', valor);
+end."""
 
 if __name__ == "__main__":
     print("=== Teste Exemplo 1 ===")
@@ -654,6 +753,7 @@ if __name__ == "__main__":
     
     print("\n=== Teste Exemplo 2 ===")
     parser.var = {}
+    parser.arrays = {}
     parser.vartype = {}
     parser.constants = {}
     parser.varcount = 0
@@ -662,12 +762,24 @@ if __name__ == "__main__":
     if result:
         print("".join(result))
     
-    print("\n=== Teste Exemplo 3 (Constantes) ===")
+    print("\n=== Teste Exemplo 3 (While complexo) ===")
     parser.var = {}
+    parser.arrays = {}
     parser.vartype = {}
     parser.constants = {}
     parser.varcount = 0
     parser.labelcount = 0
     result = parser.parse(ex3)
+    if result:
+        print("".join(result))
+    
+    print("\n=== Teste Exemplo 4 (Arrays) ===")
+    parser.var = {}
+    parser.arrays = {}
+    parser.vartype = {}
+    parser.constants = {}
+    parser.varcount = 0
+    parser.labelcount = 0
+    result = parser.parse(ex4)
     if result:
         print("".join(result))
